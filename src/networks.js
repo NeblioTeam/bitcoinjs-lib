4cc4c2a13688975d05a6a058f0a84215b5ce4ede
NeblioTeam <neblioteam@gmail.com>
2018-03-23 12:43:35 -0400

    Fix Bip32 testnet prefix

--- src/networks.js
+++ src/networks.js
@@ -142,8 +142,8 @@ var networks = {
   neblio_testnet: {
     magicPrefix: '\x18Neblio Signed Message:\n',
     bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
+      public: 0x043587cf,
+      private: 0x04358394
     },
     pubKeyHash: 0x41,
     scriptHash: 0x7f,
